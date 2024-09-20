636becfc39fec8e651f93473846224fd76338598
muhammadibrahemm <muhammadibraheem8567@gmail.com>
2024-09-20 09:36:00 +0500

    install and configure tailwind

--- client/solainity-coin-frontend/src/App.jsx
+++ client/solainity-coin-frontend/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
-import { Home } from './components/Home/Home'
 
 function App() {
 
 
   return (
     <>
-        <Home />
+         <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     </>
   )
 }
